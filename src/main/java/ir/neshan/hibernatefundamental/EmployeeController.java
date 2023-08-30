6cec5ee5eff5c520074532e98e9c80074ecea10a
amir kayyal <amir.kayyal@gmail.com>
2023-08-30 13:52:06 +0330

    add findEmployeeById method to EmployeeController class

--- src/main/java/ir/neshan/hibernatefundamental/EmployeeController.java
+++ src/main/java/ir/neshan/hibernatefundamental/EmployeeController.java
@@ -3,17 +3,30 @@ package ir.neshan.hibernatefundamental;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @AllArgsConstructor
+@RequestMapping("/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
 
-    @PostMapping("/employees")
+    @PostMapping("/create")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
     }
+
+
+    @GetMapping("/{id}")
+    public Optional<Employee> findEmployeeById(@PathVariable Long id) {
+      return employeeService.findById(id);
+    }
+
+
+
+
 
     // other endpoints for retrieving, updating, and deleting employees
 }
